a01a5cf85bde3a1b1f75fe95c779e7e0a0420882
Jvitorfrei <10756078@mackenzista.com.br>
2026-04-06 17:01:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const carrosel = document.getElementById("carrosel");
 const slides = document.getElementById("slides");
-
+const secaoProdutos = document.getElementById('popular');
 
 let index = 0;
 
@@ -20,4 +20,9 @@ function avancarIndex() {
         index++;
     }
     moverCarrosel()
+}
+
+function toggleMenu() {
+    const nav = document.getElementById('navMenu');
+    nav.classList.toggle('ativo');
 }
